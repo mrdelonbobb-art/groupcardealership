e5654f5b5884fbe4aae761dbe2874f3d7f849ca6
delon bobb <mr.delonbobb@gmail.com>
2025-10-31 12:17:19 -0400

    phase 2 in work book coplete

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -10,22 +10,47 @@ public class DealershipFileManager {
 
 
     public Dealership getDealership() {
+        Dealership theDealership = null;
         try {
             BufferedReader inventoryReader = new BufferedReader(new FileReader(filePath));
            //this reads first line of csv file
             String dealershipInfo = inventoryReader.readLine();
+
             //prove that file path works and prints first line from csv
             //System.out.println(dealershipInfo);
+
             String[] dealershipParts =dealershipInfo.split("\\|");
+
             //System.out.println(Arrays.toString(dealershipParts));
 
+            //creates dealership with parts
+          theDealership = new Dealership(dealershipParts[0],dealershipParts[1],dealershipParts[2]);
+          String vehicleInfo;
+          while( (vehicleInfo = inventoryReader.readLine())  !=null){
+              //process each vehicle
+              String[] vehicleParts = vehicleInfo.split("\\|");
+              int vin = Integer.parseInt(vehicleParts[0]);
+              int year =Integer.parseInt(vehicleParts[1]);
+              String make = vehicleParts[2];
+              String model = vehicleParts[3];
+              String vehicleType = vehicleParts[4];
+              String color = vehicleParts[5];
+              int miles = Integer.parseInt(vehicleParts[6]);
+              double price = Double.parseDouble(vehicleParts[7]);
+              //create vehicle
+              Vehicle vehicle = new Vehicle(vin,year,make,model,vehicleType,color,miles,price);
+              theDealership.addVehicle(vehicle);
 
+              {
+
+              }
+            }
         } catch (IOException e) {
             System.out.println("someting wrong bai!" + e.getMessage());
             System.exit(0);
             //do this if try fails
         }
-        return null;
+        return theDealership;
     }
 
     // saves dealership to csv file
